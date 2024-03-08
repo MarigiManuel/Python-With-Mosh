9e3334004ea6cb15df5d4660167c08aadd297cba
Marigi Manuel <marigimanuel@gmail.com>
2024-03-08 16:14:49 +0300

    Initialized

---
+++ app.py
@@ -0,0 +1,14 @@
+# A program that returns the user's name and their favorite color
+
+name = input('What is your name? ')
+color = input('What is favorite color? ')
+
+# print(f'Hello {name}, your favorite color is {color}!')
+print(name + ' likes ' + color)
+
+# Convert Pounds to Kgs
+
+weight_pounds = input('Enter your weight in pounds: ')
+weight_kilograms = int(weight_pounds) * 0.45
+
+print(weight_kilograms)
